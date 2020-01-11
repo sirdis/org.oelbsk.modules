239245e7255d0653f52358a7c35100e832b576e9
tarek <tarek@oelbsk.org>
2020-01-11 21:28:14 +0100

    remove unnecessary component scan

--- src/main/java/org/oelbsk/modules/ModulesApplication.java
+++ src/main/java/org/oelbsk/modules/ModulesApplication.java
@@ -6,16 +6,13 @@ import org.oelbsk.modules.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"org.oelbsk.modules.service"})
 public class ModulesApplication {
 
 	
 	@Autowired
-	IService service;
-	
+	IService service;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModulesApplication.class, args);
